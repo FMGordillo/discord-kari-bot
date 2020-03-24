e6227012d77ca5af48e0f01ff2868823f784d8a2
FMGordillo <facundomgordillo@gmail.com>
2020-03-24 19:35:01 -0300

    :sparkles: Core functionality

--- server.js
+++ server.js
@@ -1,4 +1,56 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
+const Discord = require("discord.js");
+const axios = require("axios").default;
 
+const app = express();
+const discordClient = new Discord.Client();
+
+const BOT_API_KEY = process.env.BOT_API_KEY;
+
+const HOST = process.env.HOST;
+const API_KEY = process.env.API_KEY;
+
+/**
+ * DISCORD STUFF
+ */
+
+discordClient.once("ready", () => console.log("BOT IS WORKING!"));
+discordClient.on("message", message => {
+  if (!message.content.startsWith("!g")) {
+    // Ignoralo :v
+  } else {
+    if (message.content.includes("champions")) {
+      axios.get("http://localhost:3000/champions").then(response => {
+        message.reply(
+          `Los ID's de campeones gratuitos de esta semana son: ${response.data.freeChampionIds}`
+        );
+      });
+    }
+    // le esta hablando al bot, hagamos cosas
+  }
+});
+
+/**
+ * EXPRESS STUFF
+ */
+
+app.get("/", (req, res) => {
+  res.send("Jelou uor :v");
+});
+
+app.get("/champions", (req, res) => {
+  axios
+    .get(`${HOST}/lol/platform/v3/champion-rotations`, {
+      headers: { "X-Riot-Token": API_KEY }
+    })
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      res.json(error);
+    });
+});
+
+discordClient.login(BOT_API_KEY);
 app.listen(3000, () => console.log(`Server running on port 3000`));
